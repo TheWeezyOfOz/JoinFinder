60eee5d1b76fd1e1f071b88a217394eb93fa2e2b
loopylouise <loopy_louise_777@hotmail.com>
2018-02-10 23:27:01 +0000

    added method to update edges in classCol

--- Utils/classCol.R
+++ Utils/classCol.R
@@ -7,7 +7,7 @@ classCol <-setRefClass(
     colPrev = "character"
   ),
   methods = list(
-    initialize = function(colName, colEdge=as.list(c()), colChek=0, colPrev)
+    initialize = function(colName, colEdge=as.list(c()), colChek=0, colPrev="")
     {
       colName <<- colName
       colEdge <<- colEdge
@@ -17,7 +17,14 @@ classCol <-setRefClass(
     hello = function()
     {
       print("hello sunshine")
+    },
+    update = function(colEdgeNew)
+    {
+      if (colEdgeNew%in%colEdge == TRUE) {
+        sprintf("%s is already in %s Node.",colEdgeNew,colName)
+      }else{
+      colEdge <<- c(colEdge,colEdgeNew)
+      }
     }
-    
   )
 )
