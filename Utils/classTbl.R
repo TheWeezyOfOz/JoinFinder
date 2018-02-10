e133f4b06aceccd151da49748f225a8b50fe7443
loopylouise <loopy_louise_777@hotmail.com>
2018-02-10 23:31:41 +0000

    added method to update edges in classTbl

--- Utils/classTbl.R
+++ Utils/classTbl.R
@@ -10,24 +10,27 @@ classTbl <- setRefClass(
     tblPrev = "character"
   ),
   methods = list(
-    initialize = function(tblDb, tblSchema, tblName, tblCols, tblEdge ,tblChek=0, tblPrev="")
+    initialize = function(tblDb, tblSchema, tblName, tblCols, tblEdge=as.list(c()) ,tblChek=0, tblPrev="")
     {
       tblDb <<- tblDb
       tblSchema <<- tblSchema
       tblName <<- tblName
       tblCols <<- tblCols
+      tblEdge <<- tblEdge
       tblChek <<- tblChek
       tblPrev <<- tblPrev
-      tblEdge <<- tblEdge
     },
     hello = function()
     {
       "hello sunshine"
     },
-    printInput = function()
+    update = function(tblEdgeNew)
     {
-      if(missing(input)) stop ("You have to provide an input")
-      print(input)
+      if (tblEdgeNew%in%tblEdge == TRUE) {
+        sprintf("%s is already in %s Node.",tblEdgeNew,tblName)
+      }else{
+        tblEdge <<- c(tblEdge,tblEdgeNew)
+      }
     }
   )
 )
