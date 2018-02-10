657712e7f83286c0d80fb69c84a2939bf3451f10
loopylouise <loopy_louise_777@hotmail.com>
2018-02-10 20:50:39 +0000

    Initial Commit of Table Class

---
+++ Utils/classTbl.R
@@ -0,0 +1,31 @@
+classTbl <- setRefClass(
+  "classTbl",
+  fields = list(
+    tblDb = "character",
+    tblSchema = "character",
+    tblName = "character",
+    tblCols = "list",
+    tblChek = "numeric",
+    tblPrev = "character"
+  ),
+  methods = list(
+    initialize = function(tblDb, tblSchema, tblName, tblCols, tblChek, tblPrev)
+    {
+      tblDb <<- tblDb
+      tblSchema <<- tblSchema
+      tblName <<- tblName
+      tblCols <<- tblCols
+      tblChek <<- tblChek
+      tblPrev <<- tblPrev
+    },
+    hello = function()
+    {
+      "hello sunshine"
+    },
+    printInput = function()
+    {
+      if(missing(input)) stop ("You have to provide an input")
+      print(input)
+    }
+  )
+)
